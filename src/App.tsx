2ec6742a4d4ebf3c37e8816197d5481a6f26175b
Tomasz Szydlowski <TSzydlowski@protonmail.com>
2022-10-13 21:58:02 +0200

    test

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import './App.css'
 
 function App() {
-
+console.log(import.meta.env.VITE_MY_TEST_SECRET)
     return (
         <div className="App">
             <h1>Lets start with Vite {import.meta.env.VITE_MY_TEST_SECRET}</h1>
