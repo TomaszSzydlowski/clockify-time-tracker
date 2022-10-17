80c5aa8603e458ff20de928e3a786e37eeb00368
Tomasz Szydlowski <TSzydlowski@protonmail.com>
2022-10-17 21:21:00 +0200

    force test

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import './App.css'
 
 function App() {
     console.log(import.meta.env.VITE_MY_TEST_SECRET)
+    console.log(import.meta.env.VITE_MY_TEST_SECRET)
     return (
         <div className="App">
             <h1>Lets start with Vite test</h1>
