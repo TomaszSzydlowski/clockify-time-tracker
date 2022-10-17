f3c296973f5a7c1257f190bd668d6af18ad56c7e
Tomasz Szydlowski <TSzydlowski@protonmail.com>
2022-10-17 21:38:27 +0200

    add msal

---
+++ src/core/auth/authProvider.tsx
@@ -0,0 +1,15 @@
+﻿import {BrowserCacheLocation, PublicClientApplication} from "@azure/msal-browser"
+
+const config = {
+    auth: {
+        authority: import.meta.env.VITE_APP_AUTHORITY as string,
+        clientId: import.meta.env.VITE_APP_CLIENT_ID as string,
+        redirectUri: import.meta.env.VITE_PUBLIC_URL as string
+    },
+    cache: {
+        cacheLocation: BrowserCacheLocation.LocalStorage,
+        storeAuthStateInCookie: false,
+    },
+}
+
+export const msalInstance = new PublicClientApplication(config)
--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,20 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
+import {MsalAuthenticationTemplate, MsalProvider} from '@azure/msal-react'
+import {msalInstance} from "./core/auth/authProvider"
+import {InteractionType} from "@azure/msal-browser"
+
+const authRequest = {scopes: ["User.Read"]}
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+        <MsalProvider instance={msalInstance}>
+            <MsalAuthenticationTemplate
+                interactionType={InteractionType.Redirect}
+                authenticationRequest={authRequest}>
+                <App/>
+            </MsalAuthenticationTemplate>
+        </MsalProvider>
+    </React.StrictMode>
 )
