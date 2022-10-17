339c7e0d466eee29196e64acd29f6cc7610930bf
Tomasz Szydlowski <TSzydlowski@protonmail.com>
2022-10-17 20:30:56 +0200

    Revert "remove pipeline"
    This reverts commit 4dcb3bf57eeaeda2c59c6a8fe62aad995f1b7d4b.

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import './App.css'
 
 function App() {
+console.log(import.meta.env.VITE_MY_TEST_SECRET)
     return (
         <div className="App">
-            <h1>Lets start with Vite {import.meta.env.VITE_MY_TEST_SECRET}</h1>
+            <h1>Lets start with Vite test:{import.meta.env.VITE_MY_TEST_SECRET}</h1>
         </div>
     )
 }
