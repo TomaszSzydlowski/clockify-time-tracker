51fb7a8c4fe53851724b0a5098b9e3090a4e90ee
Tomasz Szydlowski <TSzydlowski@protonmail.com>
2022-10-17 21:40:52 +0200

    msal test

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import './App.css'
 function App() {
     return (
         <div className="App">
-            <h1>Lets start with Vite test</h1>
+            <h1>Lets start with Vite MSAL</h1>
         </div>
     )
 }
